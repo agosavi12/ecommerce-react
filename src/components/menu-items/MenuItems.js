170ad66524f2670096829de224ba9480473e408c
Amol Gosavi <agosavi12@gmail.com>
2022-03-12 13:49:26 +0530

    smaller components  created

--- src/components/menu-items/MenuItems.js
+++ src/components/menu-items/MenuItems.js
@@ -3,12 +3,13 @@ import './MenuItems.scss'
 
 const MenuItems = ({ title, imageUrl, size }) => {
   return (
-    <div
-      style={{ backgroundImage: `url(${imageUrl})` }}
-      className={`${size} menu-item`}
-    >
+    <div className={`${size} menu-item`}>
+      <div
+        style={{ backgroundImage: `url(${imageUrl})` }}
+        className='background-image'
+      ></div>
       <div className='content'>
-        <h2 className='title'>{title}</h2>
+        <h2 className='title'>{title.toUpperCase()}</h2>
         <span className='subtitle'>Shop Now</span>
       </div>
     </div>
